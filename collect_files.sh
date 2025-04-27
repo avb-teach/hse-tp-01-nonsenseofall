77d1123e04cd65417be53a63a2f7dc76f7c2a08e
root <root@yxhchivvbp.local>
2025-04-27 17:59:29 +0000

    3 задание(1 версия)

--- collect_files.sh
+++ collect_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+#--1--
 if [ "$#" -ne 2 ]; then
     exit 1
 fi
-
+#--2--
 in_dir="$1"
 out_dir="$2"
 
@@ -11,4 +11,10 @@ mkdir -p "$out_dir"
 for f in $(find "$in_dir" -maxdepth 2 -type f); do
     cp "$f" "$out_dir/"
 done
-# 2 готово!
+
+#--3--
+#пробую отдельно для рекурсивного задания переписать, потому что не уверен, 
+#что если уберу для глубины 2, то тест 2 будет пройден корректно
+find "$in_dir" -type f | while read -r file; do #семинар+ https://habr.com/ru/companies/alexhost/articles/525394/ про find 
+    cp "$file" "$out_dir/"
+done
