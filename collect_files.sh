6f4ceac3debc96ab0205aae6c9205deacb0c0fa4
root <root@yxhchivvbp.local>
2025-04-27 16:50:04 +0000

    2 тест(1 версия)

--- collect_files.sh
+++ collect_files.sh
@@ -4,5 +4,11 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-exit 0 
 # 1 задание выполняется наконец-то верно!
+in_dir="$1"
+out_dir="$2"
+
+mkdir -p "$out_dir"
+for f in $(find "$in_dir" -maxdepth 2 -type f); do
+    cp "$f" "@$out_dir/"
+done
